236ccdb222e70ad16b496d6732b0e1510aae7251
Quang Sáng <typidane@gmail.com>
2024-09-27 15:13:01 +0700

    edit

--- static/js/app.js
+++ static/js/app.js
@@ -1,42 +1,40 @@
-$(window).on('scroll', function(){
-    if(!isMobile.any()){
-        scrolling();
-    }
-});
 
+var flag = true;
+var shift = $(window).width() > 640 ? 400 : 150;
+var active = '';
 
-$('.menu-block').on('mouseenter', function(){
+$('.menu-block').on('mouseenter', function () {
     $('.position', this).addClass('fadeInDown animated');
-    $('.position', this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    $('.position', this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
         $('.position', this).removeClass('fadeInDown animated');
     });
     $('.food-name', this).addClass('fadeInDown animated');
-    $('.food-name', this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    $('.food-name', this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
         $('.food-name', this).removeClass('fadeInDown animated');
     });
     $('.price', this).addClass('fadeInUp animated');
-    $('.price', this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    $('.price', this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
         $('.price', this).removeClass('fadeInUp animated');
     });
 
-}).on('mouseleave', function(){
+}).on('mouseleave', function () {
     $('.position', this).removeClass('fadeInDown animated');
     $('.food-name', this).removeClass('fadeInDown animated');
-    $('.price', this).removeClass('fadeInUp animated');			
+    $('.price', this).removeClass('fadeInUp animated');
 });
 
-$('.menu-block').on('click', function(){
+$('.menu-block').on('click', function () {
     active = $(this);
-    $('body').css('overflow', 'hidden');
+    // $('body').css('overflow', 'hidden');
     var img = $('img', active);
     var position = $('.position', active);
     var name = $('.food-name', active);
     var text = $('p', active);
 
-    if(active.index() == 0){				
-        $('#popup .prev').addClass('unactive');
-    }else if(active.index() == $('.menu-block').length-1 || active.index() == 15 || active.index() == 11){
-        $('#popup .next').addClass('unactive');
+    if (active.index() == 0) {
+        $('#popup button.prev').addClass('unactive');
+    } else if (active.index() == $('.menu-block').length - 1 || active.index() == 15 || active.index() == 11) {
+        $('#popup button.next').addClass('unactive');
 
     }
 
@@ -49,43 +47,45 @@ $('.menu-block').on('click', function(){
 
     $('#popup').css('opacity', 1);
 
-    setTimeout(function(){
+    setTimeout(function () {
         $('#popup article').eq(0).addClass('fadeInLeft animated').css('opacity', 1);
-        $('#popup article').eq(0).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        $('#popup article').eq(0).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
             $('#popup article').eq(0).removeClass('fadeInLeft animated');
-        });	
+        });
 
         $('#popup article').eq(1).addClass('fadeInRight animated').css('opacity', 1);
-        $('#popup article').eq(1).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        $('#popup article').eq(1).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
             $('#popup article').eq(1).removeClass('fadeInRight animated');
-        });	
-        
+        });
+
     }, 500);
-    setTimeout(function(){				
-        $('#popup .button').addClass('fadeInDown animated').css('opacity', 1);
-        $('#popup .button').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-            $('#popup .button').removeClass('fadeInDown animated');
-        });	
+    setTimeout(function () {
+        $('#popup button').addClass('fadeInDown animated').css('opacity', 1);
+        $('#popup button').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $('#popup button').removeClass('fadeInDown animated');
+        });
     }, 1000);
 });
-$('.close-button').on('click', function(){
-$('body').css('overflow', 'auto');
+$('.close-button').on('click', function () {
+    $('body').css('overflow', 'auto');
     $('#popup article, #popup .button').css('opacity', 0);
     active = '';
-    setTimeout(function(){
+    setTimeout(function () {
         $('#popup').hide()
         $('#popup .next, #popup .prev').removeClass('unactive');
     }, 500);
     return false;
 });
-$('#popup .next, #popup .prev').on('click', function(){
+$('#popup .next, #popup .prev').on('click', function () {
     var activeTemp = $(this).hasClass('prev') ? active.prev() : active.next();
-    if(!activeTemp.length || !flag){
-        if(!activeTemp.length){
-            $(this).addClass('unactive');
-        }
-        return false;
-    }
+    // if (!activeTemp.length || !flag) {
+    //     if (!activeTemp.length) {
+    //         $(this).addClass('unactive');
+    //     }
+    //     return false;
+    // }
+    console.log(flag);
+    console.log(activeTemp)
     $('#popup .next, #popup .prev').removeClass('unactive');
     flag = false;
     var img = $('img', activeTemp);
@@ -94,39 +94,38 @@ $('#popup .next, #popup .prev').on('click', function(){
     var text = $('p', activeTemp);
     $('.details-wrapper > *, #popup img').css('opacity', 0);
 
-    setTimeout(function(){
-        $('#popup img').attr('src', img.attr('src').replace(/food/, 'food/big')).css('opacity', 1);
-    }, 500);
+    // setTimeout(function () {
+    //     $('#popup img').attr('src', img.attr('src').replace(/food/, 'food/big')).css('opacity', 1);
+    // }, 500);
 
-    setTimeout(function(){
+    setTimeout(function () {
         $('#popup .position').html(position.html());
         $('#popup .food-name').html(name.html());
         $('#popup p').html(text.html());
 
         var time = 100;
-        $('.details-wrapper > *').each(function(){
+        $('.details-wrapper > *').each(function () {
             time += 200;
             var thiz = this;
-            setTimeout(function(){
-                $(thiz).addClass('fadeInDown animated').css('opacity',1);
-                $(thiz).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            setTimeout(function () {
+                $(thiz).addClass('fadeInDown animated');
+                $(thiz).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
                     $(thiz).removeClass('fadeInDown animated');
-                });						
+                });
             }, time)
         });
-        setTimeout(function(){
+        setTimeout(function () {
             flag = true;
         }, 900);
     }, 300)
-    
 
     active = activeTemp;
-
+    console.log(active)
     return false
 });
 
-$('.more-food').on('click', function(){
-    if($(this).hasClass('unactive')){
+$('.more-food').on('click', function () {
+    if ($(this).hasClass('unactive')) {
         return false;
     }
     $(this).addClass('animate');
@@ -134,42 +133,64 @@ $('.more-food').on('click', function(){
     var amount = 4;
     var width = $(window).width();
 
-    if(width <= 480){
+    if (width <= 480) {
         var amount = 4;
-    }else if(width < 768){
+    } else if (width < 768) {
         var amount = 2;
-    }else if(width < 990){
+    } else if (width < 990) {
         var amount = 3;
-    }else{
+    } else {
         var amount = 4;
     }
 
     var time = -200;
-    setTimeout(function(){
-        $('.more-food').removeClass('animate');
-        $('.menu-block.animate').each(function(i){
-            if(i >= amount){
+    setTimeout(function () {
+        $('.more-food');
+        $('.menu-block').each(function (i) {
+            if (i >= amount) {
                 return false;
             }
             time += 200;
             var thiz = this;
             $(this).show();
-            if(i == 0){
+            if (i == 0) {
                 $('html,body')
-                    .stop()
+                    // .stop()
                     .scrollTo($(this).offset().top, 300);
             }
-            setTimeout(function(){
-                $(thiz).addClass('fadeInLeft animated').removeClass('animate').css('opacity',1);
-                $(thiz).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            setTimeout(function () {
+                $(thiz).addClass('fadeInLeft animated').removeClass('animate');
+                $(thiz).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
                     $(thiz).removeClass('fadeInLeft animated');
-                });						
-                if(!$('.menu-block.animate').length){
+                });
+                if (!$('.menu-block').length) {
                     $('.more-food').addClass('unactive');
-                }					
+                }
             }, time);
         });
     }, 1000);
 
     return false;
 })
+
+if (parseInt($('#menu').offset().top) + shift < scroll) {
+    if ($('#menu').hasClass('animate')) {
+
+        $('#menu').removeClass('animate');
+
+        var time = -200;
+        $('#menu > .menu-block').each(function () {
+            time += 200;
+            var thiz = this;
+            setTimeout(function () {
+                $(thiz).addClass('fadeInLeft animated').css('opacity', 1);
+                $(thiz).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                    $(thiz).removeClass('fadeInLeft animated');
+                });
+            }, time)
+        })
+    }
+
+    $('#mainNavi a').removeClass('active');
+    $("a[href='#menu']:eq(0)").addClass('active');
+}
